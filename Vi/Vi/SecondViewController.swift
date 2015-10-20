716802640da32b8fafb014fad2398efd2333271b
Christopher Yung <cyung10892@gmail.com>
2015-10-20 09:42:11 -0700

    create project for iOS app

---
+++ Vi/Vi/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Vi
+//
+//  Created by Christopher Yung on 10/20/15.
+//  Copyright © 2015 Vi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
