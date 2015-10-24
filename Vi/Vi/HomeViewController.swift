f788024e93ef1884c5a4fadd5f6ec59a73a06891
Garrett Maring <garrett.maring@gmail.com>
2015-10-23 17:14:26 -0700

    Live rendering

--- Vi/Vi/HomeViewController.swift
+++ Vi/Vi/HomeViewController.swift
@@ -1,38 +1,111 @@
-//
-//  HomeViewController.swift
-//  Vi
-//
-//  Created by Eugene Lee (colemakdvorak) 10/20/15.
-//  Copyright © 2015 Vi. All rights reserved.
-//
 import UIKit
+import QuartzCore
 
 class HomeViewController: UIViewController {
   
+  var stt:SpeechToText = SpeechToText();
+  var listening:Bool = false
+  
+  @IBOutlet var transcript: UILabel!
+  @IBOutlet var sunButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    createButton()
+    /* Add sun button */
+    createSunPressFunctionality()
+    
+    /* Add gesture capabilities */
+    // No swipe gesturing until exact location of permitted swipe determined
+    //addUpSwipeGesture()
+    
+    /* Configure Watson */
+    configureWatson()
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func configureWatson() {
+    let conf:STTConfiguration = STTConfiguration()
+    
+    conf.audioCodec = WATSONSDK_AUDIO_CODEC_TYPE_OPUS
+    
+    conf.basicAuthUsername = "11a2a0e4-02dd-4b14-812a-bc9ec34efc3a"
+    conf.basicAuthPassword = "r5HEtX7J0tqd"
+    
+    self.stt = SpeechToText.init(config: conf)
+  }
+  
+  func addUpSwipeGesture() {
+    let upSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+    upSwipe.direction = .Up
+    view.addGestureRecognizer(upSwipe)
+  }
+  
+  func handleSwipes(sender:UISwipeGestureRecognizer) {
+    if (sender.direction == .Up) {
+      print("Swipe Up")
+      listening = !listening
+      print("Listening: \(listening)")
+      /* Start Watson Listening */
+      startListening()
+    }
+  }
+  
+  func startListening() {
+    self.stt.recognize({ (res: [NSObject:AnyObject]!, err: NSError!) -> Void in
+      
+      if err == nil {
+        if self.stt.isFinalTranscript(res) {
+          self.stt.endRecognize()
+        }
+        self.handleTranscript(self.stt.getTranscript(res))
+        self.handlePowerMeter()
+        NSLog("@%", self.stt.getTranscript(res))
+      } else {
+        self.stt.endRecognize()
+        NSLog("@%", err.localizedDescription)
+      }
+    })
+  }
+  
+  func handleTranscript(transcript: String) {
+    var oldTranscript = self.transcript.text!
+    let newTranscript = transcript
+    
+    self.transcript.text! = newTranscript
+  }
+  
+  func createSunPressFunctionality() {
+    self.sunButton.addTarget(self, action: "buttonPressed", forControlEvents: .TouchUpInside)
     
   }
   
-  func createButton () {
-    let button = UIButton();
-    button.setTitle("Vi", forState: .Normal)
-    button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-    button.frame = CGRectMake(200, 65, 46, 30) // X, Y, width, height
-    button.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
-    self.view.addSubview(button)
+  func addPulseAnimation(powerLevel: Float) {
+    let scaleAnimation:CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+    
+    scaleAnimation.duration = 0.3
+    scaleAnimation.repeatCount = 0.0
+    scaleAnimation.autoreverses = false
+    scaleAnimation.fromValue = (abs(powerLevel) / 100);
+    scaleAnimation.toValue = 1.0;
+    
+    self.sunButton.layer.addAnimation(scaleAnimation, forKey: "scale")
   }
   
-  func buttonPressed(sender: UIButton!) {
-    let alertView = UIAlertView();
-    alertView.addButtonWithTitle("Done");
-    alertView.title = "Alert!";
-    alertView.message = "Button Pressed!!!";
-    alertView.show();
+  func buttonPressed() {
+    if !listening {
+      listening = !listening
+      startListening()
+    }
   }
+  
+  func handlePowerMeter() {
+    self.stt.getPowerLevel({ (power: Float) -> Void in
+      self.addPulseAnimation(power)
+    })
+  }
+  
 }
