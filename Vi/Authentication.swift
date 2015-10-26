332720044cc6e526e9d7e4c2bc2b9787a215f84f
Garrett Maring <garrett.maring@gmail.com>
2015-10-26 16:54:54 -0700

    Single social sign on

--- Vi/Authentication.swift
+++ Vi/Authentication.swift
@@ -10,15 +10,36 @@ class Authentication: NSObject {
   }
   
   func authenticatePlatform(view: UIViewController, platform: String) {
-    let lock = Authentication.sharedInstance.lock
-    let controller = lock.newLockViewController()
-    controller.closable = true
-    controller.onAuthenticationBlock = { (profile, token) in
-      NSLog("User token: %@", token!)
-      NSLog("User profile: %@", profile!)
-      view.dismissViewControllerAnimated(true, completion: nil)
+    let newLock = Authentication.sharedInstance.lock
+//    let controller = newLock.newLockViewController()
+//    controller.closable = true
+    newLock.identityProviderAuthenticator().authenticateWithConnectionName(platform, parameters: nil, success: self.successCallback(), failure: self.errorCallback())
+    
+//    controller.onAuthenticationBlock = { (profile, token) in
+//      NSLog("User token: %@", token!)
+//      NSLog("User profile: %@", profile!)
+//      view.dismissViewControllerAnimated(true, completion: nil)
+//    }
+//    view.presentViewController(controller, animated: true, completion: nil)
+  }
+  
+  private func errorCallback() -> NSError -> () {
+    return { error in
+      let alert = UIAlertController(title: "Login failed", message: "Please check you application logs for more info", preferredStyle: .Alert)
+      alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+      self.presentViewController(alert, animated: true, completion: nil)
+      print("Failed with error \(error)")
     }
-    view.presentViewController(controller, animated: true, completion: nil)
+  }
+  
+  private func successCallback() -> (A0UserProfile, A0Token) -> () {
+    return { (profile, token) -> Void in
+      let alert = UIAlertController(title: "Logged In!", message: "User with name \(profile.name) logged in!", preferredStyle: .Alert)
+      alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+      self.presentViewController(alert, animated: true, completion: nil)
+      print("Logged in user \(profile.name)")
+      print("Tokens: \(token)")
+    }
   }
   
 }
--- Vi/ViTests-Bridging-Header.h
+++ Vi/ViTests-Bridging-Header.h
@@ -1,3 +1,4 @@
 #import <SpeechKit/SpeechKit.h>
+#import <Lock/Lock.h>
 #import "NuanceHeader.h"
 #import "AFNetworking.h"
