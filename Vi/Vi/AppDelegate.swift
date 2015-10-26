c42b112db739a5e28f25d9b0b5cddf5844124ed8
Garrett Maring <garrett.maring@gmail.com>
2015-10-26 16:26:13 -0700

    Authentication set up. plist config for auth0

--- Vi/Vi/AppDelegate.swift
+++ Vi/Vi/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+      
+      /* Authentication Set up */
+      let lock = Authentication.sharedInstance.lock
+      lock.applicationLaunchedWithOptions(launchOptions)
+      
       UIApplication.sharedApplication().statusBarStyle = .LightContent
       UINavigationBar.appearance().titleTextAttributes = [
         NSFontAttributeName: UIFont(name: "Lato-Regular", size: 24)!,
