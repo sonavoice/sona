281172a22d73be2beef863c7de16e19d073a256f
Keith Armstrong <keith1015@gmail.com>
2015-10-29 17:24:54 -0700

    added publish command

--- vipackage/utils.js
+++ vipackage/utils.js
@@ -11,4 +11,26 @@ var lodir = function(dir) {
   return path.join.apply(this, newArgs);
 };
 
+var zip = function(dir, name, cb) {
+  var output = fs.createWriteStream(lodir(name + ".zip"));
+  var archive = archiver('zip');
+
+  output.on('close', function() {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+    cb();
+  });
+
+  archive.on('error', function(err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  archive.directory(dir, name);
+
+  archive.finalize();
+};
+
 module.exports.lodir = lodir;
+module.exports.zip = zip;
