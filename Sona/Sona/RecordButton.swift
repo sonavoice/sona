c99959cf5845b5711217cc30a6b9359f347a803f
Christopher Yung <cyung10892@gmail.com>
2015-11-02 19:24:30 -0800

    fix record button circle border

--- Sona/Sona/RecordButton.swift
+++ Sona/Sona/RecordButton.swift
@@ -17,16 +17,20 @@ class RecordButton: UIButton {
   let primaryLight: UIColor = UIColor(red: 255.0/255, green: 232.0/255, blue: 235.0/255, alpha: 1)
   
   override func drawRect(rect: CGRect) {
-    let path = UIBezierPath(ovalInRect: rect)
-    primary.setFill()
-    path.fill()
-    
     let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
     let radius: CGFloat = max(bounds.width, bounds.height)/2
     
-    var arcWidth: CGFloat = radius*0.5
+    var arcWidth: CGFloat = radius-10
     let startAngle: CGFloat = 0
     let endAngle: CGFloat = 2 * PI
+    
+    let innerCircle = UIBezierPath(arcCenter: center, radius: radius-10 - (arcWidth/2), startAngle: startAngle, endAngle: endAngle, clockwise: true)
+    
+    innerCircle.lineWidth = arcWidth
+    primary.setStroke()
+    innerCircle.stroke()
+
+    arcWidth = radius * 0.5
     
     let innerRing = UIBezierPath(arcCenter: center, radius: radius - (radius/2.5)+0.5, startAngle: startAngle, endAngle: endAngle, clockwise: true)
     
@@ -37,13 +41,13 @@ class RecordButton: UIButton {
     
     arcWidth = radius * 0.14
     
-    let whiteRing = UIBezierPath(arcCenter: center, radius: radius - (arcWidth*1.0)+0.5, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+    let whiteRing = UIBezierPath(arcCenter: center, radius: radius - (arcWidth*1.0), startAngle: startAngle, endAngle: endAngle, clockwise: true)
     
     whiteRing.lineWidth = arcWidth
     UIColor.whiteColor().setStroke()
     whiteRing.stroke()
     
-    arcWidth = radius*0.07
+    arcWidth = radius*0.09
     
     let outerRing = UIBezierPath(arcCenter: center, radius: radius - (arcWidth/2), startAngle: startAngle, endAngle: endAngle, clockwise: true)
     
