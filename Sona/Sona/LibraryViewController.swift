a39c793862aba2629871bd29d2d0054fd1d91c01
Eugene Lee <colemakdvorak@gmail.com>
2015-11-06 16:37:55 -0800

    library load

--- Sona/Sona/LibraryViewController.swift
+++ Sona/Sona/LibraryViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Alamofire
 
 class LibraryViewController: UITableViewController {
-  var apps:[App]?
+  var apps:[App] = []
   var appInfo: App!
   @IBOutlet weak var menuButton: UIBarButtonItem!
   var burgerButton: HamburgerButton! = nil
@@ -17,8 +18,6 @@ class LibraryViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    self.apps = dummyData
     
     self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
     self.navigationController?.navigationBar.shadowImage = UIImage()
@@ -43,6 +42,45 @@ class LibraryViewController: UITableViewController {
     }
   }
   
+  override func viewDidAppear(animated: Bool) {
+    self.apps.removeAll()
+    self.loadApp()
+  }
+  
+  func loadApp() {
+    self.tableView.reloadData()
+    let appMan = AppManager()
+    let extensions = appMan.getAllExt()
+    for ext in extensions {
+      let name = ext.valueForKey("name")
+      Alamofire.request(.GET, "https://sonavoice.com/extension", parameters: ["name": name as! String])
+        .responseJSON { response in
+          switch response.result {
+          case .Success:
+            if let JSON = response.result.value {
+              let apps = JSON as! NSArray
+              var newApps = [App]()
+              for item in apps {
+                if self.appCheck(item) {
+                  let appName = item.valueForKey("name") as! String
+                  let appDesc = item.valueForKey("description") as! String
+                  let appComm = item.valueForKey("commands") as! [String]
+                  let appIcon = item.valueForKey("iconURL") as! String
+                  newApps.append(App(name: appName, description: appDesc, commands: appComm, iconURL: appIcon))
+                }
+              }
+              self.apps += newApps
+              self.tableView.reloadData()
+            } else {
+              
+            }
+          case .Failure:
+            print("failed")
+          }
+      }
+    }
+  }
+  
   func moveToAddExtPage() {
     self.performSegueWithIdentifier("showSearch", sender: self)
   }
@@ -52,14 +90,14 @@ class LibraryViewController: UITableViewController {
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.apps!.count
+    return self.apps.count
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
     -> UITableViewCell {
       let cell = self.tableView.dequeueReusableCellWithIdentifier("CellView", forIndexPath: indexPath) as! CellView
       
-      let appInfo = self.apps![indexPath.row] as App
+      let appInfo = self.apps[indexPath.row] as App
       cell.name.font = UIFont(name: "Lato-Regular", size: 18)
       cell.name.textColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
       cell.name.text = appInfo.name
@@ -90,7 +128,15 @@ class LibraryViewController: UITableViewController {
     if (segue.identifier == "appSegue") {
       let path = self.tableView.indexPathForSelectedRow!
       let viewController = segue.destinationViewController as! AppInfoViewController
-      viewController.appInfo = self.apps![path.indexAtPosition(1)]
+      viewController.appInfo = self.apps[path.indexAtPosition(1)]
+    }
+  }
+  
+  func appCheck(item:AnyObject) -> Bool {
+    if item.valueForKey("name") != nil && item.valueForKey("description") != nil && item.valueForKey("commands") != nil && item.valueForKey("iconURL") as! String != "fake icon URL" {
+      return true
+    } else {
+      return false
     }
   }
 }
